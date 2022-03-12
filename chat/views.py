2def0a53e29cfabcec91423b425fae23a104d988
nextLine <daniel.attitsogbe02@gmail.com>
2022-03-12 19:20:06 +0000

    prevent not authenticated users to acces rooms and room creation page

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
 from chat.models import Room, Message
 from chat.forms import NewRoomForm
 
@@ -10,19 +11,20 @@ def index(request):
     return render(request, "chat/index.html", context={"rooms" : rooms})
 
 
+@login_required(login_url="signin")
 def room(request, slug):
     room = get_object_or_404(Room, slug=slug)
     messages = Message.objects.filter(room=room)
     return render(request, "chat/room.html", context={"room" : room, "messages": messages})
 
 
+@login_required(login_url="signin")
 def new_room(request):
     if request.method == 'POST':
         form = NewRoomForm(request.POST)
 
         if form.is_valid():
             room = form.save()
-
             return redirect("room", room.slug)
     else:
         form = NewRoomForm()
