af120c3730e60e317038a0b3a27f256d235bf9d0
nextLine <daniel.attitsogbe02@gmail.com>
2022-07-08 09:18:32 +0000

    add user input verification, update user interface

--- accounts/views.py
+++ accounts/views.py
@@ -18,6 +18,20 @@ def signup(request):
         """
         username = request.POST.get("username")
         password = request.POST.get("password")
+        password2 = request.POST.get("password_confirm")
+        
+        if not username:
+            return render(request, "accounts/signup.html", {"error": "Username is required", "old_username": username})
+        
+        if User.objects.filter(username=username).exists():
+            return render(request, "accounts/signup.html", {"error": "Username already exists", "old_username": username})
+
+        if not (password and password2):
+            return render(request, "accounts/signup.html", {"error": "Password is required", "old_username": username})
+        
+        if not password == password2:
+            return render(request, "accounts/signup.html", {"error": "Passwords do not match", "old_username": username})
+        
         user = User.objects.create_user(
             username=username,
             password=password,
@@ -44,6 +58,8 @@ def signin(request):
         if user:
             login(request, user)
             return redirect("index")
+        
+        return render(request, "accounts/signin.html", {"error": "Invalid credentials", "old_username": username})
 
     return render(request, "accounts/signin.html")
 
